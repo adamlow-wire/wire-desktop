725efb5c7767dc6ec1d05d8a3f9f2970a1c5bff7
Adam Low <adam.low@wire.com>
2026-09-23 10:50:58 +0100

    fix(CAP-001): keep account preload rejection diagnostics confidential
    (cherry picked from commit 3bfa74347fe5b09360edfecff84968696aeb7113)

--- electron/src/preload/preload-account.test.main.ts
+++ electron/src/preload/preload-account.test.main.ts
@@ -14,6 +14,7 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see http://www.gnu.org/licenses/.
+ *
  */
 
 import {ModuleKind, ScriptTarget, transpileModule} from 'typescript';
@@ -71,6 +72,6 @@ describe('account preload event diagnostics (inert Node ports)', () => {
     emit({type: 'account-event'});
     await settle();
     assert.deepEqual(warnings, [['Account event rejected.']]);
-    assert.ok(!warnings.flat().includes(privateError));
+    assert.ok(!(warnings as unknown[][]).flat().includes(privateError));
   });
 });
