a28a324aeaecb1afa5ab39316f23ec07c8aa72a2
Adam Low <adam.low@wire.com>
2026-09-25 01:08:52 +0100

    fix(SEC-011): sign Wire Gov application before verification

--- jenkins/windows.groovy
+++ jenkins/windows.groovy
@@ -57,7 +57,7 @@ node('windows') {
   }
 
   stage('Sign application') {
-    if (production) {
+    if (production || wireGov) {
       withCredentials([
         string(credentialsId: 'SM_API_KEY',               variable: 'SM_API_KEY'),
         string(credentialsId: 'SM_HOST',                  variable: 'SM_HOST'),
